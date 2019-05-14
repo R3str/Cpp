ed8af720d9972eec5349037dbb435e58c50e8d39
R3str <andrey.kotoman12@gmail.com>
2019-05-14 21:37:00 +0300

    .

--- №3/№3/№3.cpp
+++ №3/№3/№3.cpp
@@ -1,5 +1,6 @@
 ﻿#include "pch.h"
 #include <iostream>
+#include <string>
 using namespace std;
 
 
@@ -7,43 +8,58 @@ int main()
 {
 	setlocale(LC_ALL, "ru");
 
-	char arr[17]{ "Один дом на луне" };
-	int numberWords;
+	string ap;
+	int numberWords;	
 	char even[20];
 	char notEven[20];
 	
 	int count = 0;
 	int position = 0;
-	int evenPosoition = 0;
+	int evenPosition = 0;
 	int notEvenPosition = 0;
 
-	cout << "Ваша строка: " << arr << endl;
+	cout << "Ваша строка: " << ap << endl;
 
-// Определение и запись четных/нечетных слов в соответствующие массивы
-	for (int i = 0; i < 17; i++)
+	getline(std::cin, ap);
+
+	while (ap.length() > 17)
 	{
-		if (arr[i] == ' ' || arr[i] == '\0')
+		cout << "Вы ввели больше 17 элементов!\n";
+		getline(std::cin, ap);
+	}
+
+	while (ap.length() < 17)
+	{
+		cout << "Вы ввели меньше 17 элементов!\n";
+		getline(std::cin, ap);
+	}
+// Определение и запись четных/нечетных слов в соответствующие массивы
+	for (int i = 0; i <= ap.length(); i++)
+	{
+		if (ap[i] == ' ' || ap[i] == '\0')
 		{
 			count++;
 			if (count % 2 == 0)
 			{
 				do
 				{
-					even[evenPosoition++] = arr[position++];
-				} while (arr[position - 1] != ' ' && arr[position - 1] != '\0');
-				even[evenPosoition-1] = ' ';
-				even[evenPosoition] = '\0';
+					even[evenPosition++] = ap[position++];
+				} while (ap[position - 1] != ' ' && ap[position - 1] != '\0');
+				even[evenPosition - 1] = ' ';
+				even[evenPosition] = '\0';
 			}
 
 			if (count % 2 != 0)
 			{
 				do
 				{
-					notEven[notEvenPosition++] = arr[position++];
-				} while (arr[position - 1] != ' ' && arr[position - 1] != '\0');
+					notEven[notEvenPosition++] = ap[position++];
+				} while (ap[position - 1] != ' ' && ap[position - 1] != '\0');
 				notEven[notEvenPosition - 1] = ' ';
 				notEven[notEvenPosition] = '\0';
 			}
+
+
 		}
 	}
 
@@ -52,17 +68,18 @@ int main()
 	numberWords = count;
 	count = 0;
 	position = 0;
-	evenPosoition = 0;
+	evenPosition = 0;
 	notEvenPosition = 0;
 
-	for (int i = 0; i < numberWords; i++)
+	for (int i = 0; i <= numberWords; i++)
 	{
 		count++;
 		if (count % 2 == 0)
 		{
 			do
 			{
-				arr[position++] = notEven[notEvenPosition++];
+				if (notEven[notEvenPosition] != '\0')
+				ap[position++] = notEven[notEvenPosition++];
 			} while (notEven[notEvenPosition-1] != ' ' && notEven[notEvenPosition-1] != '\0');
 		}
 
@@ -70,21 +87,19 @@ int main()
 		{
 			do
 			{
-				arr[position++] = even[evenPosoition++];
-			} while (even[evenPosoition-1] != ' ' && even[evenPosoition-1] != '\0');
+				if(even[evenPosition] != '\0')
+				ap[position++] = even[evenPosition++];
+			} while (even[evenPosition-1] != ' ' && even[evenPosition-1] != '\0');
 		}
 	}
 
-// Указываем конец строки
-	arr[16] = '\0';
-
 // Смена верхного регистра на нижний и начало с большого регистра 
-	for (int i = 0; i < 16; i++)
+	for (int i = 0; i < 17; i++)
 	{
-		if (int(arr[i]) > -65 && int(arr[i]) < -34)
-			arr[i] += 32;
+		if (int(ap[i]) > -65 && int(ap[i]) < -34)
+			ap[i] += 32;
 	}
-	arr[0] -= 32;
+	ap[0] -= 32;
 
-	cout << "Строка со сменой четных/нечетных слов: " << arr << endl;
+	cout << "Строка со сменой четных/нечетных слов: " << ap << endl;
 }
