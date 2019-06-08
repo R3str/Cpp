ac9c7bbdeeb6326c3d591bfd0fa6e2efa74d9d4c
R3str <andrey.kotoman12@gmail.com>
2019-06-08 13:33:13 +0300

    1

---
+++ №8/№8/№8.cpp
@@ -0,0 +1,74 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+
+struct element
+{
+	int x;
+	element *Next;
+};
+
+class List
+{
+	element *Head;
+public:
+	List() { Head = NULL; }
+	~List();
+	void Add(int x);
+	void Show();
+};
+
+List::~List()
+{
+	while (Head != NULL)
+	{
+		element *temp = Head->Next;
+		delete Head;
+		Head = temp;
+	}
+}
+
+void List::Add(int x)
+{
+	element *temp = new element;
+	temp->x = x;
+	temp->Next = Head;
+	Head = temp;
+}
+
+void List::Show()
+{
+	element *temp = Head;
+
+
+	while (temp != NULL)
+	{
+		if (temp->x < 0)
+		{
+			int i = 0;
+			while (i != 1)
+			{
+				cout << temp->x << " ";
+				temp = temp->Next;
+				i++;
+			}temp->x = 10;
+		}
+		cout << temp->x << " ";
+		temp = temp->Next;
+	}
+}
+
+int main()
+{
+	List lst;
+
+	lst.Add(46);
+	lst.Add(25);
+	lst.Add(-4);
+	lst.Add(29);
+	lst.Add(-27);
+	lst.Add(36);
+	lst.Add(54);
+
+	lst.Show();
+}
